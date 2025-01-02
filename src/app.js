b656d6b4176096af38dd152fd5629d4faa62c8be
Muhammad Sajid <sajidsmile143@gmail.com>
2025-01-03 01:54:41 +0500

    change

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 // src/app.js
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const newsRoutes = require('./routes/newsRoutes');
-const path = require('path');
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const newsRoutes = require("./routes/newsRoutes");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Serve static files
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api/auth', authRoutes);
-app.use('/api', newsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api", newsRoutes);
 
 // Export the configured app without starting it
 module.exports = app;
